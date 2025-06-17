2da1d1ae41dd4ee257f5148a8bea159ea0304395
KY020 <116252603+KY020@users.noreply.github.com>
2025-06-18 02:52:10 +0800

    write only the state change of the bottom nav bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,8 +9,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+
+  //this method will update our selected index
+  //when the user taps on the buttom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(bottomNavigationBar: MyButtomNavBar());
+    return Scaffold(
+      bottomNavigationBar: MyButtomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+    );
   }
 }
