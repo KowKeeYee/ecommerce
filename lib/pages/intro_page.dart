6fb8b045cc4609fec15520ca882bdaf4181eb3f4
KY020 <116252603+KY020@users.noreply.github.com>
2025-06-18 02:34:40 +0800

    Initial page and Bottom navBar in home page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,72 @@
+import 'package:ecommerce/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset('lib/images/nikeLogo.png', height: 240),
+              ),
+
+              const SizedBox(height: 48),
+
+              //title
+              const Text(
+                'Just Do It',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+
+              const SizedBox(height: 24),
+
+              //subtitle
+              const Text(
+                'Brand new sneakers and custom kicks made with premium quality',
+                style: TextStyle(fontSize: 20, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 48),
+
+              //start now button
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: Center(
+                    child: const Text(
+                      'Shop now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
