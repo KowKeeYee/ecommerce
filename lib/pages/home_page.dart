8784b8d5b07b87cb65c67729ed219cafce8f0bbe
KY020 <116252603+KY020@users.noreply.github.com>
2025-06-24 00:13:05 +0800

    Shop/CartPage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce/components/bottom_nav_bar.dart';
+import 'package:ecommerce/pages/cart_page.dart';
+import 'package:ecommerce/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,12 +22,22 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  //page to display
+  final List<Widget> _page = [
+    //shop page
+    const ShopPage(),
+
+    //cart page
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: MyButtomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      body: _page[_selectedIndex],
     );
   }
 }
