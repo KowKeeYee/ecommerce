cea67565de71554347f020607a90b38ea59f5d5e
KY020 <116252603+KY020@users.noreply.github.com>
2025-06-24 03:00:26 +0800

    Update shoe_tile.dart

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -9,19 +9,72 @@ class ShoeTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(left: 25),
-      width: 200,
+      width: 300,
       decoration: BoxDecoration(
         color: Colors.grey[100],
         borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           //shoe pic
-          Image.asset(shoe.imagePath),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              shoe.imagePath,
+              width: 300, // change as needed
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+          ),
 
           //description
+          Text(shoe.description, style: TextStyle(color: Colors.grey[600])),
 
           //price + details
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //shoe name
+                    Text(
+                      shoe.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+
+                    const SizedBox(height: 5),
+
+                    //price
+                    Text(
+                      '\$' + shoe.price,
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+
+                //plus button
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                    ),
+                  ),
+                  child: const Icon(Icons.add, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
 
           //button to add to cart
         ],
